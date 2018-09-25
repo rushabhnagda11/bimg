eb94d641dbc6daf5eb6d048824179ff627ce3ea8
Rushabh Nagda <rushabhnagda11@gmail.com>
2018-09-25 18:38:56 +0530

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -17,14 +17,14 @@ install_libvips_from_source() {
   echo "Compiling libvips v$vips_version_full from source"
   curl -L -o vips-$vips_version_full.tar.gz $tarbal_url
   tar zvxf vips-$vips_version_full.tar.gz
-  cd vips-$vips_version_full
+  cd libvips-$vips_version_full
 
   # Compile
   CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" ./configure --disable-debug --disable-docs --disable-static --disable-introspection --disable-dependency-tracking --enable-cxx=yes --without-python --without-orc --without-fftw $1
   make
   make install
   cd ..
-  rm -rf vips-$vips_version_latest_major_minor.$vips_version_latest_patch
+  rm -rf libvips-$vips_version_latest_major_minor.$vips_version_latest_patch
   rm vips-$vips_version_latest_major_minor.$vips_version_latest_patch.tar.gz
   ldconfig
   echo "Installed libvips $(PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --modversion vips)"
